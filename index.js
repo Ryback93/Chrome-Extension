45125eb46dc06fbb7970e0f13a14ebe42b740ca0
Ryback93 <rileybruchet@gmail.com>
2025-03-05 16:07:15 -0800

    change

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+
+const firebaseConfig = {
+    
+};
+
+const app = initializeApp(firebaseConfig);
+
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
